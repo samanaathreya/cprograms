c765fb3f8a61a07c424c23b3d9ee8973d5f6d134
athreya15n <athreya15n@gmail.com>
2022-02-05 20:24:15 +0530

    first c prog

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{   
+    char arr[20],arr1[20];
+    printf("enter: ");
+    /*hfusygfhvvdf
+    jfduygudrhbjn*/
+    gets(arr);
+    gets(arr1);
+    printf("the entered text is");
+    puts(arr);
+    
+   
+
+   
+    
+}
